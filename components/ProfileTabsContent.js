ee234be0682add42ac286e3bfb2a8c67e7351573
mamenDseid <mamelala@gmail.com>
2022-02-04 18:54:55 +0000

    vg

--- components/ProfileTabsContent.js
+++ components/ProfileTabsContent.js
@@ -1,14 +1,42 @@
 import { StyleSheet, Text, View, Image, StatusBar, SafeAreaView, TouchableOpacity } from 'react-native';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { Tab, TabView } from 'react-native-elements';
 
 
 
+export default function ProfileTabsContent() {
+    const [index, setIndex] = useState(0)
 
-export default function ProfileTabsContent(){
-    return(
+
+    return (
         <View style={styles.conte}>
 
-            <Text>hello from tabs</Text>
+            <Tab
+                value={index}
+                onChange={(e) => setIndex(e)}
+                indicatorStyle={{
+                    backgroundColor: '#F5F5F5',
+                    height: 3,
+                }}
+                variant="#F5F5F5"
+            >
+                <Tab.Item
+                   
+                    titleStyle={{ fontSize: 12 }}
+
+                    icon={{ name: 'dashboard', type: 'materialIcons', color: 'black' }}
+                />
+                <Tab.Item
+                    
+                    titleStyle={{ fontSize: 12 }}
+                    icon={{ name: 'heart', type: 'ionicon', color: 'black' }}
+                />
+                <Tab.Item
+                    
+                    titleStyle={{ fontSize: 12 }}
+                    icon={{ name: 'cart', type: 'ionicon', color: 'black' }}
+                />
+            </Tab>
         </View>
     )
 }
