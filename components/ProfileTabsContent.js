454ba600eebea908be062a22eb6d4e48db371d0b
mamenDseid <mamelala@gmail.com>
2022-02-05 17:16:56 +0000

    cv

--- components/ProfileTabsContent.js
+++ components/ProfileTabsContent.js
@@ -1,78 +1,54 @@
-import { StyleSheet, Text, View, Image, StatusBar, SafeAreaView, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
-import React, { useState, useContext } from 'react';
-import { Tab, TabView } from 'react-native-elements';
-import { CredentialsContext } from './CredentialsContext';
+import React, { useState } from 'react';
+import {
+    Text, View,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native';
+import Constants from 'expo-constants';
+
+import { Feather } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function ProfileTabsContent() {
+    const [toggletab, setToggle] = useState(0)
 
 
-
-export default function ProfileTabsContent({ userId, feeds }) {
-    const [index, setIndex] = useState(0)
-    const { storedCredentials, setStoredCredentials } = useContext(CredentialsContext)
-    const { userName, email, lastSeen, profilePic, uid, uniName } = storedCredentials
-    console.log(feeds);
-
-    const isTrue = userId === uid
+    const tabtoogle = (index) => {
+        setToggle(index)
+    }
     return (
-        <>
         <View style={styles.conte}>
+            <TouchableOpacity onPress={()=> tabtoogle(0)}>
+                <Feather name="grid" size={24} color="black" />
 
-            <Tab
-                value={index}
-                onChange={(e) => setIndex(e)}
-                indicatorStyle={{
-                    backgroundColor: '#F5F5F5',
-                    height: 3,
-                }}
-                variant="#F5F5F5"
-            >
-                <Tab.Item
+            </TouchableOpacity>
 
-                    titleStyle={{ fontSize: 12 }}
+            <TouchableOpacity  onPress={()=> tabtoogle(1)}>
+                <MaterialIcons name="view-carousel" size={24} color="black" />
 
-                    icon={{ name: 'grid-view', type: 'materialIcons', color: 'black' }}
-                />
-                <Tab.Item
+            </TouchableOpacity>
+            <TouchableOpacity  onPress={()=> tabtoogle(2)}>
 
-                    titleStyle={{ fontSize: 12 }}
-                    icon={{ name: 'view-carousel', type: 'materialIcons', color: 'black' }}
-                />
-                <Tab.Item
-
-                    titleStyle={{ fontSize: 12 }}
-                    icon={{ name: "bookmark", type: 'fontawsome', color: 'black' }}
-                />
-
-                <Tab.Item
-
-                    titleStyle={{ fontSize: 12 }}
-                    icon={{ name: "person-circle", type: 'ionicon', color: 'black' }}
-                />
-            </Tab>
-            <TabView value={index} onChange={setIndex} animationType="spring">
-
-                <TabView.Item style={{ backgroundColor: 'red', width: '100%' }}>
-                    <Text h1>Recent</Text>
-                </TabView.Item>
-                <TabView.Item style={{ backgroundColor: 'blue', width: '100%' }}>
-                    <Text h1>Favorite</Text>
-                </TabView.Item>
-                <TabView.Item style={{ backgroundColor: 'green', width: '100%' }}>
-                    <Text h1>Cart</Text>
-                </TabView.Item>
-                <TabView.Item style={{ backgroundColor: 'green', width: '100%' }}>
-                    <Text h1>Cart</Text>
-                </TabView.Item>
+                <FontAwesome name="bookmark" size={24} color="black" />
 
 
-            </TabView>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=> tabtoogle(3)}>
+                <Ionicons name="person-circle-outline" size={24} color="black" />
+
+            </TouchableOpacity>
         </View>
-        </>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-
     conte: {
-        marginTop: 25
+        marginTop: 20,
+        marginHorizontal: 20,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
     }
 });
